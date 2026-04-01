a157e5d8d8bc2994ebbb9aff0e45d6083fa14073
ericsussman <ericandrewsussman@gmail.com>
2026-04-01 18:05:12 -0400

    website redesign

--- bus_times/demo.py
+++
@@ -1,5 +0,0 @@
-def submit_input(e):
-    input_box = Element("input_box")
-    player_input = input_box.select(".value")
-    main_description = Element("main_description")
-    main_description.write(player_input)
